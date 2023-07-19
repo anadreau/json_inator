2d9fc0604a3383ac2b83d673f8b51dac32a8dd26
Alex Nadreau <anadreau42@gmail.com>
2023-07-19 06:30:57 -0700

    Merging with main.

--- lib/features/save_to_file/save.dart
+++ lib/features/save_to_file/save.dart
@@ -7,15 +7,15 @@ import 'dart:io';
 import 'package:creator/creator.dart';
 import 'package:file_picker/file_picker.dart';
 
-/// Takes a [data] object and writes it to a file at [filename]
+/// Takes a [saveData] object and writes it to a file at [filename]
 final saveFile = Creator<void>((ref) async {
   var fileName = await ref.read(getDirectoryCreator) ?? 'No Path Selected';
-  var raw = ref.read(data);
+  var raw = ref.read(saveData);
   File file = File(fileName);
   file.writeAsStringSync(jsonEncode(raw));
 }, name: 'saveFile');
 
-final data = Creator((ref) => {'title': 'Save test', 'subject': 'testing'},
+final saveData = Creator((ref) => {'title': 'Save test', 'subject': 'testing'},
     name: 'Data');
 
 final getDirectoryCreator = Creator((ref) async {
