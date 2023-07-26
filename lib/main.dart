0f974c4c7d0ab0d33f1dae7c6881e8a9c9070df3
Alex Nadreau <anadreau42@gmail.com>
2023-07-26 14:37:58 -0700

    added imbedded todo for issue8

--- lib/main.dart
+++ lib/main.dart
@@ -95,38 +95,36 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const Expanded(flex: 3, child: JsonViewer()),
-            const Divider(),
-            const Flexible(child: FractionallySizedBox(heightFactor: .1)),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Raw Json'),
-                Watcher((context, ref, child) =>
-                    Text(ref.watch(saveData).toString()))
-              ],
-            ),
-            const Flexible(child: FractionallySizedBox(heightFactor: .1)),
-          ],
-        ),
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          const Expanded(flex: 3, child: JsonViewer()),
+          const Divider(),
+          const Flexible(child: FractionallySizedBox(heightFactor: .1)),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Raw Json'),
+              Watcher(
+                  (context, ref, child) => Text(ref.watch(saveData).toString()))
+            ],
+          ),
+          const Flexible(child: FractionallySizedBox(heightFactor: .1)),
+        ],
       ),
     );
   }
